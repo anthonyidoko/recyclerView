b51945709578d5b9077a4d5b7bf32479d28233ce
anthonyidoko <idokoanthony86@gmail.com>
2021-07-30 03:15:06 +0100

    created the recycler  data class and passed in the properties

---
+++ app/src/main/java/com/example/classwork/RecyclerData.kt
@@ -0,0 +1,8 @@
+package com.example.classwork
+
+import android.widget.ImageView
+
+data class RecyclerData(
+    var image: Int, var name: String,
+    var phoneNumber :String, var email :String
+    )
